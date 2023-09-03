0888f5ef236b6efe0e7f6ed7287d6d4048c99442
NyroY64 <youcef.meradi09@gmail.com>
2023-09-04 01:12:15 +0200

    made the grid interactive
    you can write on the grid by hovering over but it's not the purpose I need the correct it

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ function drawGrid(dimensions) {
     for (let i = 0; i < (dimensions**2) ; i++) {
         let div=document.createElement('div');
         div.setAttribute('class','grid-items');
+        
+        div.addEventListener('mouseover',()=>{
+            div.style['background-color']= 'black';
+        })
+        div.addEventListener('click',()=>{
+            div.style['background-color']= 'black';
+        })
+
         sketchArea.appendChild(div);
     }
 }
 
 drawGrid(16);
+
