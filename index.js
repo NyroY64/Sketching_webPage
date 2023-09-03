725dd328f862610eeff4e338c524b2ddf64f7909
NyroY64 <youcef.meradi09@gmail.com>
2023-09-04 00:38:59 +0200

    add a grid layout to simulate drawing area automate the JS to create the grid (use a for loop to add div's for every pixel) add some styling to make the grid layout more defined

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const sketchArea= document.querySelector('.sketch');
+const options=document.querySelector('.options');
 
+
+function drawGrid(dimensions) {
+    for (let i = 0; i < (dimensions**2) ; i++) {
+        let div=document.createElement('div');
+        div.setAttribute('class','grid-items');
+        sketchArea.appendChild(div);
+    }
+}
+
+drawGrid(16);
