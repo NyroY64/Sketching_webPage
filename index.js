19cfd8eb213b531e8ab60500a2a24ce480cc9ad4
NyroY64 <youcef.meradi09@gmail.com>
2023-09-10 19:56:42 +0200

    make the grid work better
    -had an issue with the grid only working with a drag or a hover
    *used a boolean to check for mouse down

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const sketchArea= document.querySelector('.sketch');
 const options=document.querySelector('.options');
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
+//drawing section 
 function drawGrid(dimensions) {
+    sketchArea.style.gridTemplateColumns =`repeat(${dimensions},1fr)`;
+    sketchArea.style.gridTemplateRows =`repeat(${dimensions},1fr)`;
+
     for (let i = 0; i < (dimensions**2) ; i++) {
         let div=document.createElement('div');
         div.setAttribute('class','grid-items');
         
-        div.addEventListener('mouseover',()=>{
-            div.style['background-color']= 'black';
-        })
-        div.addEventListener('click',()=>{
-            div.style['background-color']= 'black';
-        })
+        div.addEventListener('mouseover',changeColor);
+        div.addEventListener('mousedown',changeColor);
+  
 
         sketchArea.appendChild(div);
     }
+}
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor='#333333';
+    
 }
 
 drawGrid(16);
